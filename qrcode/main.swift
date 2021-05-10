e715438604557ae154959dcbb4c3374c444f4969
Marcus Schappi <marcus@schappi.com>
2021-05-10 12:39:01 +1000

    initial

--- qrcode/main.swift
+++ qrcode/main.swift
@@ -6,6 +6,34 @@
 //
 
 import Foundation
+import CoreImage
+import Cocoa
 
-print("Hello, World!")
+func detectQRCode(fileName : URL) -> [CIFeature]? {
+    if let ciImage = CIImage(contentsOf: fileName){
+    let context = CIContext()
+    let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+    let qrDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options)
+    let features = qrDetector?.features(in: ciImage, options: options)
+    return features
+}
+    return nil
+}
 
+let args = CommandLine.arguments
+
+if args.count > 1 {
+    let inputURL = URL(fileURLWithPath: args[1])
+   
+    if let features = detectQRCode(fileName : inputURL), !features.isEmpty{
+        for case let row as CIQRCodeFeature in features{
+            print(row.messageString ?? "nope")
+        }
+        
+    }
+    
+    exit(EXIT_SUCCESS)
+} else {
+    fputs("Error - Not enough arguments\n", stderr)
+    exit(EXIT_FAILURE)
+}
